393dd700708c3a87bbd0cc8f60793eeec8fd012a
j-berg <j-berg@users.noreply.github.com>
2021-01-21 20:27:03 -0700

    Fix bulk build script

--- build_mvdb.sh
+++ build_mvdb.sh
@@ -21,15 +21,15 @@ cd $SCRDIR
 
 # Activate conda environment
 source /uufs/chpc.utah.edu/common/home/u0690617/miniconda3/etc/profile.d/conda.sh
-conda remove -y -n metaboverse_cli
-conda create -y -n metaboverse_cli
+#conda remove -y -n metaboverse_cli
+#conda create -y -n metaboverse_cli
 source activate metaboverse_cli
-conda config --add channels bioconda
-conda config --add channels conda-forge
-conda install -y python
-conda install -y conda
-conda install -y pyinstaller
-conda install -y pandas numpy scipy scikit-learn matplotlib networkx requests
+#conda config --add channels bioconda
+#conda config --add channels conda-forge
+#conda install -y python
+#conda install -y conda
+#conda install -y pyinstaller
+#conda install -y pandas numpy scipy scikit-learn matplotlib networkx requests
 
 # Build current version of metaboverse-cli
 pyinstaller $MY_PATH/metaboverse-linux.spec
@@ -42,12 +42,12 @@ REACOME_API="https://reactome.org/ContentService/data/species/all"
 SPECIES=( $( curl -s $REACOME_API | jq -r '.[].abbreviation' ) )
 echo 'Processing database curation for: '
 for X in ${SPECIES[@]};
-  do echo '${X}' ;
+  do mkdir -p $SCRDIR/${X} ;
 done
 
 # Run
 echo "+ Running scripts..."
-parallel "$MY_PATH/dist/metaboverse-cli-linux -o $SCRDIR -s " "{}" ::: "${SPECIES[@]}"
+parallel "$MY_PATH/dist/metaboverse-cli-linux -o $SCRDIR/" "{}" " -s " "{}" ::: "${SPECIES[@]}"
 echo "+ Processing complete..."
 
 echo "+ Outputing metadata"
